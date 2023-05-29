090167ee145ba9bfde044d44443eceb7fedf5f53
Sarathdev <sarathdev0@gmail.com>
2023-05-29 20:22:50 +0530

    database connection established

--- learnly_backend/app.js
+++ learnly_backend/app.js
@@ -1,17 +1,21 @@
 import express from 'express';
+import db from './config/dbConnect.js'
 
 const app = express();
 
 
 
 
+db();
+
 const PORT = process.env.PORT || 5000
 
 
-
+app.get("/" , (req,res) =>{
+  res.send("hey")
+} )
 
 app.listen(PORT ,()=>{
   console.log(`Server Running on Port${PORT}`);
 })
 
-export {app}
