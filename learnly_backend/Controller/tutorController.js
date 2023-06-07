f35baf1abebd383de0f9d82f03b81beba9c74685
Sarathdev <sarathdev0@gmail.com>
2023-06-07 18:00:05 +0530

    tutor auth controller setup

--- learnly_backend/Controller/tutorController.js
+++ learnly_backend/Controller/tutorController.js
@@ -5,11 +5,15 @@ import bcrypt from 'bcrypt'
 const secret_key = process.env.SECRET_KEY
 const maxAge = 3* 24 * 60 * 60; 
 
+
+// create a jwt token 
 const createToken = (id)=> {
   return jwt.sign({id} , secret_key ,{
     expiresIn : maxAge
   });
 }
+
+
 
 export async function tutorLogin (req, res) {
 
@@ -21,6 +25,7 @@ try {
     return res.json({ login : false , message :"User not Exist "})
   }
 
+  // verify the password
   const validPassword = bcrypt.compare(password , tutor.password);
 
   if (!validPassword) {
@@ -37,3 +42,33 @@ try {
 }
 
 }
+
+export function tutorAuth (req , res) {
+  
+try {
+  const authHeader = req.headers.authorization ;
+  if (!authHeader) {
+    return res.json({status : false , message : "Not Authorized" })
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token , secret_key , async(err , decoded) => {
+    if(err) {
+      res.json({ status : false , message : "Permission not allowed"})
+    }else {
+      const tutor = Tutor.findOne({_id :decoded.id , status:true})
+
+      if (tutor){
+        res.json({status : true , message : " Authorized"})
+      }else {
+        res.json({status : false , message : "tutor not exists"})
+      }
+    }
+  });
+} catch (err) {
+  res.json({ status : false , message : err.message})
+}
+
+}
+
