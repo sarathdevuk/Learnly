38c090fa77ae5fa4c9e8407e12a4ef2635bf01ee
Sarathdev <sarathdev0@gmail.com>
2023-06-11 23:42:16 +0530

    tutor change password done using formik library

--- client/src/Componants/Tutor/ChangePassword.jsx
+++ client/src/Componants/Tutor/ChangePassword.jsx
@@ -3,6 +3,7 @@ import { useFormik , Formik } from "formik";
 
 import * as Yup from 'yup' ;
 import { ToastContainer , toast } from "react-toastify";
+import { changePassword } from "../../services/tutorApi";
 
 
 
@@ -19,18 +20,37 @@ import { ToastContainer , toast } from "react-toastify";
       .oneOf([Yup.ref('passwod') , null] , 'Password and ConfirmPassword must match' )
     })
 
-    // const formik = useFormik({
-    //   initialValues : {
-    //     oldpassword : "",
-    //     password : "",
-    //     confirmpassword : "",
-    //   },
-    //   validationSchema: validate,
-    //   onSubmit:async (values) => {
+    const formik = useFormik({
+      initialValues : {
+        oldpassword : "",
+        password : "",
+        confirmpassword : "",
+      },
+      validationSchema: validate,
+      onSubmit:async (values) => {
+        changePassword(values).then((resposne)=> {
+          if(resposne.status) {
+            toast.success(resposne.data.message , {
+              position:"top-center" ,
+            });
+          }
+        }) 
+        .catch((resposne) => {
+          toast.error(resposne.message , {
+            position : "top-center",
+          }); 
+        })
       
-    //   }
-    
-    // })
+      }
+    })
+
+    const handleChange = (event) => {
+      formik.setValues((prev) => {
+        const formFields = {...prev} ;
+        formFields[event.target.name] = event.target.value ;
+        return formFields 
+      })
+    }
 
 
   return (
@@ -45,19 +65,33 @@ import { ToastContainer , toast } from "react-toastify";
           <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5 " >
             <div>
               <label htmlFor="oldpassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Old Password </label>
-              <input  type="password" name="password" id="password"  placeholder="Old Password"  className="bg-gray-50 border border-gray-300 text-grey-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              <input  type="password" name="password" id="password"  placeholder="Old Password"  className="bg-gray-50 border border-gray-300 text-grey-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+               onChange={() => { handleChange(event) }}/>
+
+               {formik.touched.oldpassword && formik.errors.oldpassword ? (
+                <div>{formik.errors.oldpassword}</div>
+               ) : null}
              </div>
 
               <div>
               <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-              <input type="password" name="password" id="password" placeholder="New Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+              <input type="password" name="password" id="password" placeholder="New Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={() => { handleChange(event) }}
+              />
+              {formik.touched.password && formik.errors.password ? (
+              <div className='text-red-500' >{formik.errors.password}</div>
+                ) : null}
               
               </div>
               <div>
               <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-              <input type="confirm-password" name="confirmpassword" id="confirm-password" placeholder="Confirm Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              <input type="confirm-password" name="confirmpassword" id="confirm-password" placeholder="Confirm Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={() => { handleChange(event) }}
+              />
 
-
+              {formik.touched.confirmpassword && formik.errors.confirmpassword ? (
+              <div className='text-red-500' >{formik.errors.confirmpassword}</div>
+               ) : null}
 
               </div>
 
