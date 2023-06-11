507d651312e05aedb498dd21b3d9c2cee693a4e2
Sarathdev <sarathdev0@gmail.com>
2023-06-11 20:33:11 +0530

    tutor change password routes setup done

---
+++ client/src/Componants/Tutor/ChangePassword.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+// import { useFormik , Formik } from "formik";
+
+// import * as Yup from 'yup' ;
+// import { ToastContainer , toast } from "react-toastify";
+
+
+  function ChangePassword (){
+
+
+  return (
+    <div className="h-screen flex justify-center items-center ">
+      <div className="flex flex-col items-center justify-center mx-auto w-full lg:py-0">
+        <div className="w-full p-6 bg-white rounded-lg ">
+
+          <h2>
+            Change Password
+          </h2>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+
+}
+
+export default ChangePassword 
--- client/src/routes/TutorRouter.jsx
+++ client/src/routes/TutorRouter.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Route , Routes } from "react-router-dom";
 import TutorDashboard from "../Pages/Tutor/TutorDashboard";
 import TutorLoginPage from "../Pages/Tutor/TutorLoginPage";
+import ChangePasswordPage from "../Pages/Tutor/ChangePasswordPage";
 
 function TutorRouter() {
   return (
 
     <Routes >
       <Route path="/dashboard" element= {<TutorDashboard/>} /> 
-      <Route path="/" element= {<TutorLoginPage/>} /> 
-     
+      <Route path="/" element= {<TutorLoginPage/>} />
+      <Route path="/change-password" element ={<ChangePasswordPage/>}/>
+
     </Routes>
   )
 }
