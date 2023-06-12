4155758a301c320412efcc20aede5a599e98ac7f
Sarathdev <sarathdev0@gmail.com>
2023-06-12 12:16:52 +0530

    tutor error page done

--- client/src/routes/TutorRouter.jsx
+++ client/src/routes/TutorRouter.jsx
@@ -3,14 +3,21 @@ import { Route , Routes } from "react-router-dom";
 import TutorDashboard from "../Pages/Tutor/TutorDashboard";
 import TutorLoginPage from "../Pages/Tutor/TutorLoginPage";
 import ChangePasswordPage from "../Pages/Tutor/ChangePasswordPage";
+import PrivateRoutes from "../utils/PrivateRoutes"; 
+import ErrorPage from "../Componants/ErrorPage/ErrorPage";
 
 function TutorRouter() {
   return (
 
-    <Routes >
-      <Route path="/dashboard" element= {<TutorDashboard/>} /> 
-      <Route path="/" element= {<TutorLoginPage/>} />
+    <Routes > 
+
+      <Route element={<PrivateRoutes role={"tutor"} route={'/tutor'} />} >
+       <Route path="/dashboard" element= {<TutorDashboard/>} /> 
       <Route path="/change-password" element ={<ChangePasswordPage/>}/>
+
+      </Route>
+      <Route path="/*" element={<ErrorPage/>} />
+      <Route path="/" element= {<TutorLoginPage/>} />
 
     </Routes>
   )
