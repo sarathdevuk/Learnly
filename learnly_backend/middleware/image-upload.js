dbc1b3ba3e10a3e11b1542ed5fc98168565a95bc
Sarathdev <sarathdev0@gmail.com>
2023-06-15 12:04:49 +0530

    added filefilter in multer

--- learnly_backend/middleware/image-upload.js
+++ learnly_backend/middleware/image-upload.js
@@ -1,14 +1,25 @@
 import multer from 'multer' ;
 
+
+const fileFilter = ( req , file , cb ) => {
+  if(file.mimetype == 'image/jpeg' ||
+     file.mimetype == 'image/png'     ) {
+    cb(null , true )
+  }else{
+    cb(null , false)
+  }
+}
+
 export default function uploadImage (path) {
   const storage = multer.diskStorage({
     destination : function (req , file , cb) {
       cb(null , path ) ;
     },
     filename : function (req , file , cb) {
-      cb(null , Date.now() + '_' )
+      cb(null , Date.now() + '-' + file.originalname)
     }
   })
 
-  return multer({ storage }).fields([{ name : 'image' , maxCount : 1}])
+  return multer({ storage , fileFilter  }).fields([{ name : 'image' , maxCount : 1}])
 }
+    
