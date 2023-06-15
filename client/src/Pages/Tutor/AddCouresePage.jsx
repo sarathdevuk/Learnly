4e403e9c3d290696e706fddb7de0a06eb7f089c2
Sarathdev <sarathdev0@gmail.com>
2023-06-15 13:25:52 +0530

    implemented header and sidebar for add course

--- client/src/Pages/Tutor/AddCouresePage.jsx
+++ client/src/Pages/Tutor/AddCouresePage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from '../../Componants/Header/AdminHeader'
+import Sidebar from '../../Componants/Sidebar/Sidebar'
+import AddCourse from '../../Componants/Tutor/AddCourse/AddCourse'
 
 function AddCouresePage() {
   return (
-    <div>AddCouresePage</div>
+    <div className='relative'>
+        <Sidebar tutor={true} />
+        <Header role={'tutor'} />
+        <div className='admin-page p-3'>
+          <AddCourse/>
+        </div>
+    </div>
   )
 }
 
