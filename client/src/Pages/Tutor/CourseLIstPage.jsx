6cec694e77b06b9ebd9a525f875a902514f94ba5
Sarathdev <sarathdev0@gmail.com>
2023-06-16 19:40:32 +0530

    Somany modal bug fixes

--- client/src/Pages/Tutor/CourseLIstPage.jsx
+++ client/src/Pages/Tutor/CourseLIstPage.jsx
@@ -1,64 +1,141 @@
-    
+import React from "react";
 
-    import React from 'react'
-    
-    function CourseLIstPage() {
-      return (
-//         <div>   
-//      <button data-modal-target="large-modal" data-modal-toggle="large-modal" className="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-//         Large modal
-//         </button>
-//         <div id="large-modal" tabIndex="-1" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-//     <div className="relative w-full max-w-4xl max-h-full">
-//         {/* <!-- Modal content --> */}
-//         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-//             {/* <!-- Modal header --> */}
-//             <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
-//                 <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-//                     Large modal
-//                 </h3>
-//                 <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="large-modal">
-//                     <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-//                     <span className="sr-only">Close modal</span>
-//                 </button>
-//             </div>
-//             {/* <!-- Modal body --> */}
-//             <div className="p-6 space-y-6">
-//                 <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-//                     With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-//                 </p>
-//                 <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-//                     The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-//                 </p>
-//             </div>
-//             {/* <!-- Modal footer --> */}
-//             <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-//                 <button data-modal-hide="large-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-//                 <button data-modal-hide="large-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-//             </div>
-//         </div>
-//     </div>
-// </div>
-        
-//         </div>
+function CourseLIstPage() {
+  return (
+    <div>
+      {/* You can open the modal using ID.showModal() method */}
+      <button className="btn" onClick={() => window.my_modal_3.showModal()}>
+        open modal
+      </button>
+      <dialog id="my_modal_3" className="modal ">
+        <div className="">
+        <form
+          method="dialog"
+          className="modal-box  fixed top-0 left-0 z-[1055] h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        >
+            
+          <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+            <h5
+              className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+              id="exampleModalXlLabel"
+            >
+              Add Chapter
+            </h5>
 
-        <div>
-            {/* The button to open modal */}
-<label htmlFor="my_modal_6" className="btn">open modal</label>
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </div>
+          <div className='p-7'>
+                            <div className='flex  mt-5 '>
+                                <div className="relative mb-3 w-full md:w-1/2 m-3" data-te-input-wrapper-init>
+                                    <input
+                                        type="text"
+                                        className="peer block min-h-[auto] w-full rounded border-gray-300  bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                        id="chapterName"
+                                        name='chapterName'
+                                        // value={chapter}
+                                        // onChange={(e) => { handleLessonChange(e); setChapter(e.target.value) }}
+                                        placeholder="Form control lg" />
 
-{/* Put this part before </body> tag */}
-<input type="checkbox" id="my_modal_6" className="modal-toggle" />
-<div className="modal">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">This modal works with a hidden checkbox!</p>
-    <div className="modal-action">
-      <label htmlFor="my_modal_6" className="btn">Close!</label>
-    </div>
-  </div>
-</div>
+                                    {/* {lessonFormik.touched.chapterName && lessonFormik.errors.chapterName ? (
+                                        <p className="text-red-500 text-xs ">{lessonFormik.errors.chapterName}</p>
+                                    ) : null} */}
+
+                                    <label
+                                        htmlFor="chapterName"
+                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.7rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+                                    >Chapter Name
+                                    </label>
+                                </div>
+
+                            </div>
+
+                            <div className='flex  mt-5 '>
+                                <div className="relative mb-3 w-full md:w-1/2 m-3" data-te-input-wrapper-init>
+                                    <input
+                                        type="text"
+                                        className="peer block min-h-[auto] w-full rounded border-gray-300  bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                        id="name"
+                                        
+                                        name='lessonName'
+                                        placeholder="Name" />
+                                    {/* {lessonFormik.touched.lessonName && lessonFormik.errors.lessonName ? (
+                                        <p className="text-red-500 text-xs ">{lessonFormik.errors.lessonName}</p>
+                                    ) : null} */}
+                                    <label
+                                        htmlFor="name"
+                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.7rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+                                    >Lesson Name
+                                    </label>
+                                </div>
+                                <div className="relative mb-3 w-full md:w-1/2 sm:w-1/1 m-3" data-te-input-wrapper-init>
+                                    <input
+                                        type="text"
+                                        name='videoUrl'
+                                        // onChange={(e) => { handleLessonChange(e) }}
+                                        // value={lessonFormik.values.videoUrl}
+                                        className="peer block min-h-[auto] w-full rounded border-gray-300  bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                        id="videoUrl"
+                                        placeholder="Video Url" />
+                                    {/* {lessonFormik.touched.videoUrl && lessonFormik.errors.videoUrl ? (
+                                        <p className="text-red-500 text-xs ">{lessonFormik.errors.videoUrl}</p>
+                                    ) : null} */}
+                                    <label
+                                        htmlFor="videoUrl"
+                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.7rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+                                    >Video Link
+                                    </label>
+                                </div>
+                                <div className="relative mb-3 w-full md:w-1/3 m-3" data-te-input-wrapper-init>
+                                    <button type="button" className="focus:outline-none text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-400 
+                                    dark:hover:bg-green-500 dark:focus:ring-green-500"
+                                        // onClick={lessonFormik.handleSubmit}
+                                    >Add</button>
+                                </div>
+
+                            </div>
+
+                                <div>
+                                    <div>
+                                        <h1 className='ml-4 mt-3'>Lessons</h1>
+                                    </div>
+
+                                    <div className='flex flex-wrap'>
+                                 
+                                                    <div className='p-3 w-full md:w-1/2'>
+                                                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                                                <h5 className=" text-lg font-semibold tracking-tight text-gray-900 dark:text-white"><span className='mr-3'>1.</span>java</h5>
+
+                                                            </div>
+                                                        </a>
+
+                                                    </div>
+                                          
+                                    </div>
+
+                                    <div className="flex flex-wrap -mx-3 mb-2">
+
+                                        <div className='mt-8 w-full  flex justify-end mr-7'>
+                                            <button type="button"
+                                                className='loading-btn form-btn mt-2 font-medium rounded' >
+                                                <span className="txt">
+                                                    Submit
+                                                </span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                           
+                        </div>
+
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </form>
         </div>
-      )
-    }
-    
-    export default CourseLIstPage
+      </dialog>
+    </div>
+  );
+}
+
+export default CourseLIstPage;
