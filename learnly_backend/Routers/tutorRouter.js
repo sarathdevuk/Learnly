fc6847a36f354e57f47ffa1d70591d165d635202
Sarathdev <sarathdev0@gmail.com>
2023-06-19 11:49:38 +0530

    Set Route for getcourse and deleteCourse

--- learnly_backend/Routers/tutorRouter.js
+++ learnly_backend/Routers/tutorRouter.js
@@ -1,18 +1,22 @@
 import express from "express";
 import {  changePassword, tutorAuth, tutorLogin } from "../Controller/tutorController.js";
 import { verifyTutorLogin } from "../middleware/AuthTutor.js";
-import { addCourse } from "../Controller/courseController.js";
+import { addCourse, deleteCourse, getCourse } from "../Controller/courseController.js";
 import  uploadImage  from "../middleware/image-upload.js";
 
 
 const router = express.Router()
 
+// Auth
 router.get("/auth" , tutorAuth);
-
 router.post("/login" , tutorLogin);
 
+// Change Password
 router.put('/change-password', verifyTutorLogin  , changePassword);
 
+// Course Management
 router.post('/add-course' , verifyTutorLogin , uploadImage('./public/images/course/thumbnail') , addCourse  )
+router.get('/course' , verifyTutorLogin ,getCourse)
+router.delete('/delete-course/:courseId' , verifyTutorLogin , deleteCourse) ;
 
 export default router
