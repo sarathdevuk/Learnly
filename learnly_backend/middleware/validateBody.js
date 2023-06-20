d3f074ed8d6139111b517ac16b9e16b92763207a
Sarathdev <sarathdev0@gmail.com>
2023-06-20 20:39:58 +0530

    created body validation

---
+++ learnly_backend/middleware/validateBody.js
@@ -0,0 +1,16 @@
+import asyncHandler from 'express-async-handler' ;import { Schema } from 'mongoose';
+
+
+
+const validate = ( Schema ) => asyncHandler(async(req,res,next) => {
+  try {
+    req.body = await Schema.validate(req.body , { stripUnknown :true });
+    next();
+  } catch (error) {
+    console.log(error);
+    // you need to make an middleware for error handler 
+    res.json({status :false ,  message : error.message })
+  }
+})
+
+export default validate
