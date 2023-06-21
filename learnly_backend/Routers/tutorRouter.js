7c817b2fd45bf8be5abef24a0bf9c01f62e056db
Sarathdev <sarathdev0@gmail.com>
2023-06-21 10:04:10 +0530

    Tutor login validation added from the server side

--- learnly_backend/Routers/tutorRouter.js
+++ learnly_backend/Routers/tutorRouter.js
@@ -3,20 +3,25 @@ import {  changePassword, tutorAuth, tutorLogin } from "../Controller/tutorContr
 import { verifyTutorLogin } from "../middleware/AuthTutor.js";
 import { addCourse, deleteCourse, getCourse } from "../Controller/courseController.js";
 import  uploadImage  from "../middleware/image-upload.js";
+import validate from "../middleware/validateBody.js";
+import { loginSchema } from "../utils/yupSchema.js";
 
 
 const router = express.Router()
 
 // Auth
 router.get("/auth" , tutorAuth);
-router.post("/login" , tutorLogin);
+router.post("/login" , validate(loginSchema)  , tutorLogin);
+
+// Verifyning the Authorization ;
+router.use(verifyTutorLogin)
 
 // Change Password
-router.put('/change-password', verifyTutorLogin  , changePassword);
+router.put('/change-password' , changePassword);
 
 // Course Management
-router.post('/add-course' , verifyTutorLogin , uploadImage('./public/images/course/thumbnail') , addCourse  )
-router.get('/course' , verifyTutorLogin ,getCourse)
-router.delete('/delete-course/:courseId' , verifyTutorLogin , deleteCourse) ;
+router.post('/add-course' , uploadImage('./public/images/course/thumbnail') , addCourse  )
+router.get('/course' ,getCourse)
+router.delete('/delete-course/:courseId' , deleteCourse) ;
 
 export default router
--- learnly_backend/utils/yupSchema.js
+++ learnly_backend/utils/yupSchema.js
@@ -6,5 +6,5 @@ export const loginSchema = yup.object().shape({
   email:yup.string().trim().required()
   .test("isvalidEmail", "Invalid e-Mail", (arg) => /[a-z0-9]+@[a-z0-9]+.com/i.test(arg)),
   
-  password : yup.string().trim().required().min(3).max(16),
+  password : yup.string().trim().required().min(6).max(16),
 })
