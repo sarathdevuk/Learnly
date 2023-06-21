e9ac06b6f3cf21c1f6fc5a0ae51e4210dd9c9d95
Sarathdev <sarathdev0@gmail.com>
2023-06-21 12:08:38 +0530

    List Course tableHeader set dynamically

--- client/src/Pages/Admin/CourseListPage.jsx
+++ client/src/Pages/Admin/CourseListPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../Componants/Header/AdminHeader'
 import Sidebar from '../../Componants/Sidebar/Sidebar'
 import Table from '../../Componants/Table/Table'
 
 function CourseListPage() {
+
+  const [ course , setCourse ] =  useState();
+
+
+  const tableHeader = [
+    {title: "Title"},
+    {title: "Tutor"},
+    {title: "Duration"},
+    {title: "Status"},
+    {title: "Action"},
+  ]
+
   return (
    
     <div> 
       <Sidebar admin={true} /> 
       <Header role={'admin'} /> 
-          <Table/>  
+        <div className='admin-page p-3'>
+          <Table tableHeader={tableHeader} type={'Course'}/>  
+        </div>
+
       </div>
   )
 }
