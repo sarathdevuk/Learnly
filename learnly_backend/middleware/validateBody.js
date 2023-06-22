737742e1d3ac6270d8beb65fed73b3a361329101
Sarathdev <sarathdev0@gmail.com>
2023-06-22 11:29:58 +0530

    handled validation error using erromiddleware

--- learnly_backend/middleware/validateBody.js
+++ learnly_backend/middleware/validateBody.js
@@ -1,15 +1,15 @@
 import asyncHandler from 'express-async-handler' 
 
-
-
 const validate = ( Schema ) => asyncHandler(async(req,res,next) => {
   try {
     req.body = await Schema.validate(req.body , { stripUnknown :true });
     next();
   } catch (error) {
-    console.log(error);
+    
     // you need to make an middleware for error handler 
-    res.json({status :false ,  message : error.message })
+    res.status(400)
+    throw new Error(error.message)
+   
   }
 })
 
