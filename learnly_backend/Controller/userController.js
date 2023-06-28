5fa9330b89b9ecbd64c7e28c758cbfbe2d681551
Sarathdev <sarathdev0@gmail.com>
2023-06-28 14:53:11 +0530

    updateUserprofile Controller created

---
+++ learnly_backend/Controller/userController.js
@@ -0,0 +1,29 @@
+import User from "../models/userModel.js";
+
+export async function getUserDetails(req, res) {
+  console.log("get userDetails");
+  try {
+    const userDetails = await User.findById(
+      { _id: req.userId },
+      { password: 0 }
+    );
+    res.status(200).json({ userDetails });
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Internal server Error" });
+  }
+}
+export async function updateUserProfile(req, res) {
+  try {
+    const { firstName, lastName } = req.body;
+
+    const updatedUser = await User.updateOne(
+      { _id: req.userId },
+      { $set: { firstName, lastName } }
+    );
+    res
+      .status(200)
+      .json({ status: true, message: "Profile updated Successfully" });
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Internal server Error" });
+  }
+}
