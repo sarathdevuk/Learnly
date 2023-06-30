806ebe5627a772a7e9df5fc7ea955aef8a37bcab
Sarathdev <sarathdev0@gmail.com>
2023-06-30 16:14:14 +0530

    CourseDetailspage created

---
+++ client/src/Pages/User/CourseDetailsPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserHeader from '../../Componants/User/UserHeader/UserHeader'
+import Footer from '../../Componants/User/Footer/Footer'
+import CourseDetails from '../../Componants/User/CourseDetails/CourseDetails'
+
+function CourseDetailsPage() {
+  return (
+    <div>
+      <UserHeader/>
+      <CourseDetails/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default CourseDetailsPage
