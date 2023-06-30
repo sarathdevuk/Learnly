1d257243743815f136af4c6f68328c57ed697d8b
Sarathdev <sarathdev0@gmail.com>
2023-06-30 16:15:15 +0530

    View Course Details route changed in ViewAllCourse

--- client/src/Componants/User/CourseCard/CourseCard.jsx
+++ client/src/Componants/User/CourseCard/CourseCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function CourseCard( {course }) {
   return (
     <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <Link to={`/course-details/${course._id}`}>
+    <Link to={`/course/${course._id}`}>
         <img className="rounded-t-lg w-full  object-cover " style={{ height: "174px" }} src={import.meta.env.VITE_SERVER_URL+course.image.path}  />
         <div className="p-4">
             <h5 className="mb-2 text-lg font-bold tracking-tigsht text-theme-color dark:text-white">{course.name}</h5>
