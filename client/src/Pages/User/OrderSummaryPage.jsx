b04299a0150684033a3de0763b58c5ea716e4b79
Sarathdev <sarathdev0@gmail.com>
2023-07-01 14:03:50 +0530

    orderSummary ui created

---
+++ client/src/Pages/User/OrderSummaryPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserHeader from '../../Componants/User/UserHeader/UserHeader'
+import Footer from '../../Componants/User/Footer/Footer'
+import OrderSummary from '../../Componants/User/OrderSummary/OrderSummary'
+
+
+function OrderSummaryPage() {
+  return (
+   <>
+    <UserHeader/>
+    <OrderSummary/>
+    <Footer/>
+
+   </>
+  )
+}
+
+export default OrderSummaryPage
