6708e96a5953a2889750c1110934664d1970d5b1
Sarathdev <sarathdev0@gmail.com>
2023-07-03 20:04:19 +0530

    Chenages Made in Enrolled course

--- client/src/Componants/User/HorizontalCourseCard/HorizontalCourseCard.jsx
+++ client/src/Componants/User/HorizontalCourseCard/HorizontalCourseCard.jsx
@@ -16,7 +16,7 @@ function HorizontalCourseCard({ courseDetails }) {
                 dispatch(setCourseDetails({ ...courseDetails,courseInfo:{...courseDetails.course}, course }))
                 navigate(`/course/learn/${courseDetails.course._id}`)
             }} className="flex justify-center mt-4 sm:mx-10 m-3">
-                <div className="flex p-4 w-full max-w-screen-lg hover:bg-violet-50 flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div className="flex p-4 w-full max-w-screen-lg hover:bg-violet-50 flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md md:flex-row  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <img className="rounded-md mt-4 sm:mt-0 w-80 h-40 md:w-56 md:h-32 object-cover" src={import.meta.env.VITE_SERVER_URL + courseDetails.course.image.path}  />
                     <div className="flex flex-col ml-0 sm:ml-3 justify-between mt-2 sm:0 p-4 leading-normal">
                         <h5 className="mb-2 text-xl  font-bold tracking-tight text-gray-900 dark:text-white">{courseDetails.course.name}</h5>
--- client/src/Pages/User/EnrolledCoursePage.jsx
+++ client/src/Pages/User/EnrolledCoursePage.jsx
@@ -1,64 +1,70 @@
-import React , {useState , useEffect} from 'react'
-import UserHeader from '../../Componants/User/UserHeader/UserHeader'
+import React, { useState, useEffect } from "react";
+import UserHeader from "../../Componants/User/UserHeader/UserHeader";
 
-import Loader from '../../Componants/User/Loader/Loader'
-import { getEnrolledCourse } from '../../services/userApi';
-import { toast } from 'react-toastify';
-import HorizontalCourseCard from '../../Componants/User/HorizontalCourseCard/HorizontalCourseCard';
-
+import Loader from "../../Componants/User/Loader/Loader";
+import { getEnrolledCourse } from "../../services/userApi";
+import { toast } from "react-toastify";
+import HorizontalCourseCard from "../../Componants/User/HorizontalCourseCard/HorizontalCourseCard";
+import Footer from "../../Componants/User/Footer/Footer";
 
 function EnrolledCoursePage() {
-  const [enrolledCourse  , setEnrolledCourse ] = useState();
-  const [loading  , setLoading ] = useState(true); 
+  const [enrolledCourse, setEnrolledCourse] = useState();
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=> {
-    getEnrolledCourse().then((response) => {
-        if(response.data.status) {
-          setEnrolledCourse(response.data.enrolledCourse)
-          setLoading(false)
-        } 
-    })
-      .catch((err) => {
-        toast.error("Oops.. Something went wrong " , {
-          position: "top-center"
-        })
+  useEffect(() => {
+    getEnrolledCourse()
+      .then((response) => {
+        if (response.data.status) {
+          setEnrolledCourse(response.data.enrolledCourse);
+          setLoading(false);
+        }
       })
-  })
-
+      .catch((err) => {
+        toast.error("Oops.. Something went wrong ", {
+          position: "top-center",
+        });
+      });
+  });
 
   return (
-     <> 
-     <UserHeader/>
-        <div className='pl-9 mb-10'>
+    <>
+      <UserHeader />
+      <div className=" pl-9 md:pl-16 lg:pl-28  mb-10   ">
+        <h3 className="text-2xl md:text-2xl subpixel-antialiased   uppercase font-semibold tracking-wider mt-8  mb-4   ">
+          My Courses
+        </h3>
+      </div>
+       {/* <div className='pl-9 mb-10'>
         <h3 className="text-3xl sm:text-4xl  mt-8  mb-4 ml-2 sm:ml-5 ">My Enrollments</h3>
-      </div> 
+      </div> */}
 
-      {loading ?
+      {loading ? (
         <Loader />
-        :
-        <div>
-          {enrolledCourse.length ?
+      ) : (
+        <div className="mb-10">
+          {enrolledCourse.length ? (
             <>
-              {enrolledCourse && enrolledCourse.map((courseDetails) => {
-                return (
-                  <HorizontalCourseCard key={courseDetails._id} courseDetails={courseDetails} />
-                )
-              })
-              }
+              {enrolledCourse &&
+                enrolledCourse.map((courseDetails) => {
+                  return (
+                    <HorizontalCourseCard
+                      key={courseDetails._id}
+                      courseDetails={courseDetails}
+                    />
+                  );
+                })}
             </>
-            :
-            <div className='flex justify-center flex-col items-center mb-10'>
+          ) : (
+            <div className="flex justify-center flex-col items-center mb-10">
               <img src="/images/noCourse.svg" alt="" />
               <p>No enrolled courses</p>
             </div>
-          }
+          )}
         </div>
-      }
-
-
-
-     </>
-  )
+      )}
+      <Footer />
+    </>
+  );
 }
 
-export default EnrolledCoursePage
+export default EnrolledCoursePage;
