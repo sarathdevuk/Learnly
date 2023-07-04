45db1e903128d6bbaf6039d90f97a70e015770c3
Sarathdev <sarathdev0@gmail.com>
2023-07-04 13:19:00 +0530

    created seperate page for rendering Learn

---
+++ client/src/Pages/User/LearnPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Learn from '../../Componants/User/Learn/Learn'
+
+
+function LearnPage() {
+  return (
+    <React.Fragment>
+      <Learn/>
+    </React.Fragment>
+  )
+}
+
+export default LearnPage
