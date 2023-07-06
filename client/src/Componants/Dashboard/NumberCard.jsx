9b765871dc5032490841e8a107f9693f6ca4b598
Sarathdev <sarathdev0@gmail.com>
2023-07-06 10:55:08 +0530

    Created Number card componant for dashboard

---
+++ client/src/Componants/Dashboard/NumberCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NumberCard({ icon,data,color,title}) {
+  return (
+      <div className="flex items-center p-8 bg-white shadow rounded-lg shadow-lg ">
+          <div className={`${color} inline-flex flex-shrink-0 items-center justify-center h-16 w-16  rounded-full mr-6`}>
+              {icon}
+          </div>
+          <div>
+              <span className="block text-2xl font-bold">{data}</span>
+              <span className="block text-gray-500">{title}</span>
+          </div>
+      </div>
+  )
+}
+
+export default NumberCard
