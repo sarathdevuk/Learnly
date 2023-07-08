d45435615eb3539abc871b20df7bc289b7283988
Sarathdev <sarathdev0@gmail.com>
2023-07-08 22:55:16 +0530

    Search course api created

--- client/src/Componants/User/Search/Search.jsx
+++ client/src/Componants/User/Search/Search.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import { searchCourse } from "../../../services/userApi";
+import { toast } from "react-toastify";
 
 function Search() {
+  const [query, setQuery] = useState("");
+  const [course, setCourse] = useState();
+  const inputRef = useRef();
+  const [loading, setLoading] = useState(true);
+
+  const handleSearch = () => {
+    try {
+      if(query != "") {
+        searchCourse(query).then((response) => {
+          setCourse( response.data.result)
+        }).catch((err)=> {
+          console.log(err);
+          toast.error("Something went wrong" , { position: "top-center" , toastId: "Error"})
+        })
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong" , { position: "top-center" , toastId: "Error"})
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return (
     <div>
       <div className="flex flex-col p-2 py-6 mb-6 m-h-screen items-center">
@@ -9,17 +38,17 @@ function Search() {
           style={{ top: 5 }}
         >
           <input
-            // ref={inputRef}
-            // value={query}
-            // onChange={(e) => {
-            //   setQuery(e.target.value);
-            // }}
+            ref={inputRef}
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
             className="  rounded-full w-full py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:shadow-outline lg:text-sm text-xs"
             type="text"
             placeholder="Search"
           />
           <div
-            // onClick={handleSearch}
+            onClick={handleSearch}
             className="bg-violet-500 p-2 hover:bg-violet-700 cursor-pointer mx-2 rounded-full"
           >
             <svg
@@ -38,13 +67,44 @@ function Search() {
         </div>
       </div>
 
-      < div className='mx-3 lg:mx-20 mb-8 text-center'>
-             <h3 className='ml-5 text-2xl md:text-3xl'>Search what do you want to learn</h3>
+      {course ? (
+        <div className="mx-3 lg:mx-20 mb-8">
+          <h3 className="ml-5 text-2xl md:text-3xl">
+            {course ? course.length : " "} results for “{query}”
+          </h3>
         </div>
-
-        <div className='flex justify-center items-center'>
-                    <img src="/images/noCourse.svg" className="w-1/2 h-1/2" alt="" />
+      ) : (
+        <div className="mx-3 lg:mx-20 mb-8 text-center">
+          <h3 className="ml-5 text-2xl md:text-3xl">
+            Search what do you want to learn
+          </h3>
+        </div>
+      )}
+     {course && course.length ? course.map((obj, index) => {
+                return (
+                    <Link to={`/course-details/${obj._id}`}>
+                        <div key={index} className='mx-3 lg:mx-20 mb-10'>
+                            <div className="flex justify-center mt-4 sm:mx-10 m-3">
+                                <div className="flex p-4 w-full max-w-screen-lg hover:bg-violet-50 flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                    <img className="rounded-md mt-4 sm:mt-0 w-56 h-32 object-cover" src={import.meta.env.VITE_SERVER_URL + obj.image.path} />
+                                    <div className="flex flex-col ml-0 sm:ml-3 justify-between mt-2 sm:0 p-4 leading-normal">
+                                        <h5 className="mb-2 text-xl  font-bold tracking-tight text-gray-900 dark:text-white">{obj.name}</h5>
+                                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{obj.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                )
+            })
+                :
+                <div className='flex justify-center items-center'>
+                    <img src="/images/noCourse.svg" className="w-1/2 h-1/2" alt="No Course" />
                 </div>
+            }
+
+
+
     </div>
   );
 }
