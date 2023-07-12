a234080886eef6701c63752486fa199164d2e864
Sarathdev <sarathdev0@gmail.com>
2023-07-12 12:33:55 +0530

    Added dropdown Assignment  with Download feature

--- client/src/Componants/User/SyllabusDropdown/SyllabusDropdown.jsx
+++ client/src/Componants/User/SyllabusDropdown/SyllabusDropdown.jsx
@@ -1,8 +1,26 @@
 
 import './SyllabusDropdown.scss'
+import React from 'react';
 
-function SyllabusDropdown({ course, index, toggleDropdown, getYoutubeVideoId }) {
-    console.log("course from SyllabusDropdown " , course);
+
+function SyllabusDropdown({ course, index, toggleDropdown, getYoutubeVideoId , isCourseEnrolled }) {
+    console.log("course from SyllabusDropdown " , course.assignments.secure_url);
+
+    const handleDownload = async (imageUrl) => {
+        try {
+          const response = await fetch(imageUrl);
+          const blob = await response.blob();
+      
+          const downloadLink = document.createElement('a');
+          downloadLink.href = URL.createObjectURL(blob);
+          downloadLink.download = 'image.jpg'; // Set the desired file name
+      
+          downloadLink.click();
+        } catch (error) {
+          console.log('Error occurred during image download:', error);
+        }
+      };
+
     return (
         <div
             className={"syllabus   " + (course.open ? "open" : "")}
@@ -37,10 +55,29 @@ function SyllabusDropdown({ course, index, toggleDropdown, getYoutubeVideoId }) 
                                 </svg>
                                 <p className='ml-2 lesson-content-type text-slate-500'>Video</p>
                             </div>
+                            
                         </div>
+                        
                     )
+                    
                 })
+               
+                
             }
+               <div className={"lessons-title cursor-pointer  hover:bg-violet-50 " + (course.open ? "p-4" : "")}>
+               <p className='lesson-title'> Assignments </p>
+                 <div className='flex items-center text-slate-500 mt-1'>
+                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke="currentColor" className="w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M10.75 14.5v-9M10.75 14.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                 </svg> 
+                        <p  
+                        onClick={() => isCourseEnrolled ? handleDownload(course.assignments.secure_url) : ""} 
+                        className='ml-2 lesson-content-type text-slate-500'>download file</p>
+                      
+                </div>
+               </div>
+               
+
         </div>
     )
 }
