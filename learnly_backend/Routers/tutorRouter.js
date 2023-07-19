408cbc75e95ba91b0786361f6b0f97940b3a6bec
Sarathdev <sarathdev0@gmail.com>
2023-07-19 17:28:01 +0530

    Updated Tutor Routes For Replay Question Patch  Route

--- learnly_backend/Routers/tutorRouter.js
+++ learnly_backend/Routers/tutorRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {  changePassword, getDashboardDetails, tutorAuth, tutorLogin } from "../Controller/tutorController.js";
 import { verifyTutorLogin } from "../middleware/AuthTutor.js";
-import { EditCourseDetails, addCourse, deleteCourse, getCourse} from "../Controller/courseController.js";
+import { EditCourseDetails, addCourse, deleteCourse, getCourse, replayQuestion} from "../Controller/courseController.js";
 import  uploadImage  from "../middleware/image-upload.js";
 import validate from "../middleware/validateBody.js";
 import { loginSchema } from "../utils/yupSchema.js";
@@ -27,6 +27,9 @@ router.post('/add-course' , uploadImage('./public/images/course/thumbnail')  , a
 router.get('/course' ,getCourse)
 router.delete('/delete-course/:courseId' , deleteCourse) ;
 router.put('/update-course' ,  uploadImage('./public/images/course/thumbnail' , ) ,EditCourseDetails)
+
+//Question 
+router.patch('/replay-question/:id' , replayQuestion )
 
 // Dashboard
 router.get('/dashboard' , getDashboardDetails)
