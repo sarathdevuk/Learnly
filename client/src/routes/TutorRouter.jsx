1b476edc05e46e1b17cf7fdda39c6b3b2881adf8
Sarathdev <sarathdev0@gmail.com>
2023-07-19 17:32:00 +0530

    Updated Tutor Router for Rendering ViewQuestionPage and ReplyQuestionPage

--- client/src/routes/TutorRouter.jsx
+++ client/src/routes/TutorRouter.jsx
@@ -8,6 +8,8 @@ import ErrorPage from "../Componants/ErrorPage/ErrorPage";
 import AddCouresePage from "../Pages/Tutor/AddCouresePage";
 import ViewCoursePage from "../Pages/Tutor/ViewCoursePage";
 import EditCoursePage from "../Pages/Tutor/EditCoursePage";
+import ViewQuestionsPage from "../Pages/Tutor/ViewQuestionsPage";
+import ReplayQuestionPage from "../Pages/Tutor/ReplayQuestionPage";
 
 function TutorRouter() {
   return (
@@ -17,7 +19,9 @@ function TutorRouter() {
         <Route path="/add-course" element={<AddCouresePage />} />
         <Route path="/edit-course/:courseId"  element={<EditCoursePage />} />{" "}
         <Route path="/course" element={<ViewCoursePage />} />
-        <Route path="/change-password" element={<ChangePasswordPage />} />
+        <Route path="/questions" element={<ViewQuestionsPage />} />
+        <Route path="/questions/replay/:courseId" element={<ReplayQuestionPage />} />
+        <Route path="/change-password" element={<ChangePasswordPage />} /> 
       </Route>
       <Route path="/*" element={<ErrorPage />} />
       <Route path="/" element={<TutorLoginPage />} />
