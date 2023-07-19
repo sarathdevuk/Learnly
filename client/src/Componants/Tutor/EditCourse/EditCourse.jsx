b94e4c799d66d4b3590c7d321c23e5d0073039bc
Sarathdev <sarathdev0@gmail.com>
2023-07-19 18:55:55 +0530

    Add Assignment input design updated in  Add Course & Edit Course  page

--- client/src/Componants/Tutor/EditCourse/EditCourse.jsx
+++ client/src/Componants/Tutor/EditCourse/EditCourse.jsx
@@ -783,6 +783,8 @@ function EditCourse() {
               </div>
 
               <div className="mx-4 mt-3   md:mx-20">
+              <div  className="block  tracking-wide text-slate-800 text-md font-semibold mb-2"
+                     >ADD Assignment</div>  
                 <input
                   className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400  focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                   aria-describedby="file_input_help"
