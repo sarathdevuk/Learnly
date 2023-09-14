e90f2e46dc3580f81f0744f2778e026c1d0efbaa
Sarathdev <sarathdev0@gmail.com>
2023-09-14 19:42:36 +0530

    Group Schema Created

---
+++ learnly_backend/models/groupModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const GroupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: Object,
+      required: true,
+    },
+    members: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    Community: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Communitys",
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const groupModel = mongoose.model("Groups", GroupSchema);
+
+export default groupModel;
