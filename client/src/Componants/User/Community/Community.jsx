dea16d33008e84b595367a1555b751d023269863
Sarathdev <sarathdev0@gmail.com>
2023-09-15 21:58:43 +0530

    Added Create Modal and listed the joined community with images

--- client/src/Componants/User/Community/Community.jsx
+++ client/src/Componants/User/Community/Community.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "../Button/Button";
 import { useSelector } from "react-redux";
+import { BiPlus } from 'react-icons/bi';
 import {
   getCommunity,
   getJoinedCommunity,
@@ -8,6 +9,11 @@ import {
   joinCommunity,
 } from "../../../services/userApi";
 import { toast } from "react-toastify";
+import GroupCard from "../GroupCard/GroupCard";
+import Loader from '../Loader/Loader'
+import AddCommunityModal from "./AddCommunityModal/AddCommunityModal";
+
+
 
 function Community({ isTab }) {
   const user = useSelector((state) => state.user);
@@ -75,9 +81,57 @@ function Community({ isTab }) {
       setYourCommunity(response.data.userDetails.community);
     });
   }, []);
+  console.log( communitys );
+
 
   return (
-    <div>
+    <div
+      className={`w-full ${
+        isTab ? "px-2 py-0" : "border-x px-5 py-3 sm:px-8"
+      } border-base-300 `}
+    >
+      {/* community user have joinde */}
+      {user.id && joinedCommunity.length > 0 ? (
+        <>
+          <div className="flex justify-between items-center mb-5">
+            <h1 className="text-xl font-bold sm:text-2xl">Your Community</h1>
+            {user.email ? (
+              <Button
+                onClick={() => {
+                  setShowModal(true);
+                }}
+              >
+                <div
+                  className="flex justify-center items-center"
+                  data-te-toggle="modal"
+                  data-te-target="#editCourse"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  <BiPlus size={22} />{" "}
+                  <span className="ml-4 hidden md:flex">Create New</span>
+                </div>
+              </Button>
+            ) : null}
+          </div>
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="mt-3 grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+              {joinedCommunity.map((community) => (
+                <GroupCard
+                  key={community._id}
+                  community={community}
+                  joined={true}
+                />
+              ))}
+            </div>
+          )}
+        </>
+      ) : (
+        ""
+      )}
+
       {/* list of all community */}
       <div className="pb-16">
         <div className="flex justify-between items-center mt-10 mb-5">
@@ -107,7 +161,42 @@ function Community({ isTab }) {
             ""
           )}
         </div>
+
+        {communitys.length > 0 ? (
+          <div className="my-3 grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+            <>
+              {communitys.map((community) => (
+                <GroupCard
+                  key={community._id}
+                  joinedStatus={
+                    yourCommunity && yourCommunity.includes(community._id)
+                  }
+                  community={community}
+                  handleJoin={handleJoin}
+                />
+              ))}
+            </>
+          </div>
+        ) : (
+          <div className="mt-16 sm:mt-0 flex-col flex justify-center items-center ">
+            <img
+              className="max-w-[400px] lg:max-w-[600px] "
+              src="/images/community-empty.gif"
+              alt=""
+            />
+            <h1>No Community found</h1>
+          </div>
+        )}
       </div>
+      {showModal ? (
+        <AddCommunityModal
+          loadAllCommunityDetails={loadAllCommunityDetails}
+          loadJoinedCommunity={loadJoinedCommunity}
+          close={() => {
+            setShowModal(false);
+          }}
+        />
+      ) : null}
     </div>
   );
 }
