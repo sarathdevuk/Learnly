8a7bb14ed55099581fc06bc70f7c55b421b7fd89
Sarathdev <sarathdev0@gmail.com>
2023-09-15 16:41:00 +0530

    group slice created

---
+++ client/src/Redux/Features/groupSlice.js
@@ -0,0 +1,31 @@
+import {createSlice} from '@reduxjs/toolkit';
+import { fetchAllJoinedGroups } from '../Actions/groupActions'; 
+
+const initialState = {
+  groups: [] ,
+  status : 'idle' ,
+  error : null 
+}
+
+const groupSlice = createSlice({
+  name : "group",
+  initialState,
+  extraReducers : (builder) => {
+    builder
+         .addCase(fetchAllJoinedGroups.pending , (state) => {
+          state.status = 'loading'
+         })
+         .addCase(fetchAllJoinedGroups.fulfilled, (state, action) => {
+          state.status = true
+          state.groups = action.payload.groups
+         })
+         .addCase(fetchAllJoinedGroups.rejected , (state , action) => {
+          state.status = 'failed';
+          state.error = action.error.message ;
+         })
+  }
+})
+
+export const { setGroupDetails } = groupSlice.actions ;
+
+export default groupSlice.reducer ;
