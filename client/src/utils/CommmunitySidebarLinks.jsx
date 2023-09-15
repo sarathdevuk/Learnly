6b47a2275c88e7e7993fbab2f3c710cdaab21987
Sarathdev <sarathdev0@gmail.com>
2023-09-15 16:42:51 +0530

    Community sidebar link created

---
+++ client/src/utils/CommmunitySidebarLinks.jsx
@@ -0,0 +1,25 @@
+import { BiHomeAlt } from "react-icons/bi";
+import { MdOutlineExplore, MdOutlineMessage, MdEvent } from "react-icons/md";
+
+
+const CommunitySidebarLink = [
+  {
+      label: "Home",
+      icon: <BiHomeAlt size={22} />,
+      to: "/community",
+  },
+  {
+      label: "Explore groups",
+      icon: <MdOutlineExplore size={22} />,
+      to: "/groups",
+  },
+  {
+      label: "Messages",
+      icon: <MdOutlineMessage size={22} />,
+      to: "/messages",
+  },
+  
+];
+
+export default CommunitySidebarLink
+
