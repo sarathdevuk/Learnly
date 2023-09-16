87a768fba2ff4f09e13e719b1c4e9db235391d43
Sarathdev <sarathdev0@gmail.com>
2023-09-16 12:48:23 +0530

    Card Loading for loading group

---
+++ client/src/Componants/User/CardLoading/CardLoading.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function CardLoading() {
+    return (
+        <div className="containers mt-5 px-3  mx-auto">
+  <div className="flex flex-wrap -m-4">
+    <div className="p-2 w-full md:w-2/4 lg:w-1/3">
+      <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+        <div className="lg:h-48 bg-gray-200 md:h-36 w-full object-cover object-center" />
+        <div className="p-6">
+          <h2 className="bg-gray-400 animate-pulse h-4 w-1/4 mb-2" />
+          <h1 className="w-1/2 mb-4 h-6 animate-pulse bg-gray-500" />
+          <p className="leading-relaxed mb-3 w-full h-3 animate-pulse bg-gray-400" />
+          <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-gray-400" />
+        </div>
+      </div>
+    </div>
+    <div className="p-2 w-full md:w-2/4 lg:w-1/3">
+      <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+        <div className="lg:h-48 bg-gray-200 md:h-36 w-full object-cover object-center" />
+        <div className="p-6">
+          <h2 className="bg-gray-400 animate-pulse h-4 w-1/4 mb-2" />
+          <h1 className="w-1/2 mb-4 h-6 animate-pulse bg-gray-500" />
+          <p className="leading-relaxed mb-3 w-full h-3 animate-pulse bg-gray-400" />
+          <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-gray-400" />
+        </div>
+      </div>
+    </div>
+    <div className="p-2 w-full md:w-2/4 lg:w-1/3">
+      <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+        <div className="lg:h-48 bg-gray-200 md:h-36 w-full object-cover object-center" />
+        <div className="p-6">
+          <h2 className="bg-gray-400 animate-pulse h-4 w-1/4 mb-2" />
+          <h1 className="w-1/2 mb-4 h-6 animate-pulse bg-gray-500" />
+          <p className="leading-relaxed mb-3 w-full h-3 animate-pulse bg-gray-400" />
+          <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-gray-400" />
+        </div>
+      </div>
+    </div>
+    <div className="p-2 w-full md:w-2/4 lg:w-1/3">
+      <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+        <div className="lg:h-48 bg-gray-200 md:h-36 w-full object-cover object-center" />
+        <div className="p-6">
+          <h2 className="bg-gray-400 animate-pulse h-4 w-1/4 mb-2" />
+          <h1 className="w-1/2 mb-4 h-6 animate-pulse bg-gray-500" />
+          <p className="leading-relaxed mb-3 w-full h-3 animate-pulse bg-gray-400" />
+          <p className="leading-relaxed mb-3 w-2/3 h-3 animate-pulse bg-gray-400" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    )
+}
+
+export default CardLoading
