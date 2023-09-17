b280655bd0f0bde9865228672137cfb0c25df0cf
Sarathdev <sarathdev0@gmail.com>
2023-09-17 23:27:00 +0530

    Community Tab created for dynamically rendering tabs

---
+++ client/src/utils/communityTab.js
@@ -0,0 +1,9 @@
+const CommunityTab = [
+  { label : "Feed" , order : 1} ,
+  { label : "Groups" , order :2 },
+  { label : "Members" , order :3 },
+  { label : "Description" , order :4 }
+]
+
+
+export default CommunityTab
